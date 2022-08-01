a51340ddae9161bd2d414bfc9075bc0d6047c50b
Matthew S <matthewjschiro@gmail.com>
2022-08-01 20:45:58 +0000

    scratch1 reverse

--- projects/scratch-pad/day-1/homework/reverse-string.js
+++ projects/scratch-pad/day-1/homework/reverse-string.js
@@ -18,13 +18,21 @@
  *  c. how do you access individual characters of a String?
  *  d. how do you concatenate Strings? What operator do we use?
  */
-
+/*
+I: function takes in an input string
+O: function returns a new string representing the input string reversed
+C: N/A
+E: N/A
+*/
 function reverseString(input) {
     // YOUR CODE GOES BELOW HERE //
+    // create an empty string
     var reversed = "";
+    // loop through the input string in reverse and put values into the empty string
     for (var i = input.length -1; i >= 0; i--) {
       reversed += input[i];
     }
+    // return the new reversed string
     return reversed;
     
     // YOUR CODE GOES ABOVE HERE //
