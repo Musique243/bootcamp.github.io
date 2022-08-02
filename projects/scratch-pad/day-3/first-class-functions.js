c27fe81ec46a76566a6be055134d057fc0ae6580
Matthew S <matthewjschiro@gmail.com>
2022-08-02 21:01:10 +0000

    scratch3 first

--- projects/scratch-pad/day-3/first-class-functions.js
+++ projects/scratch-pad/day-3/first-class-functions.js
@@ -11,8 +11,15 @@
  * return a Function that tests whether a given value is greater than the 
  * base.
  */
+/*
+I: function takes in an input string or number named base
+O: function returns a function that tests whether a value is greater than the input
+C: N/A
+E: N/A
+*/
 function createGreaterThanFilter(base) {
     // YOUR CODE BELOW HERE //
+    // return a function that tests if a value is greater than input base
     return function(value) {
         return value > base;
     }
@@ -27,8 +34,15 @@ function createGreaterThanFilter(base) {
  * return a Function that tests whether a given value is less than the 
  * base.
  */
+/*
+I: function takes in an input string or number called base
+O: function returns a function that tests if a value is less than the input base
+C: N/A
+E: N/A
+*/
 function createLessThanFilter(base) {
     // YOUR CODE BELOW HERE //
+    // return a function that tests if value is less than the base
     return function(value) {
         return value < base;
     }
@@ -43,8 +57,15 @@ function createLessThanFilter(base) {
  * Function that tests whether a given String starts with the startsWith 
  * character.
  */
+/*
+I: function takes in a single character
+O: function returns a function that tests if a string starts with the character
+C: needs to be same case
+E: N/A
+*/
 function createStartsWithFilter(startsWith) {
     // YOUR CODE BELOW HERE //
+    // return a function that tests if a string starts with the character
     return function(string) {
         return string[0].toLowerCase() === startsWith.toLowerCase();
     }
@@ -59,8 +80,15 @@ function createStartsWithFilter(startsWith) {
  * Function that tests whether a given String ends with the endsWith 
  * character.
  */
+/*
+I: function takes in a single character
+O: function returns a function that tests if a string ends with the character
+C: needs to be same case
+E:
+*/
 function createEndsWithFilter(endsWith) {
     // YOUR CODE BELOW HERE //
+    // return a function that tests if string ends with the character
     return function(string) {
         return string[string.length - 1].toLowerCase() === endsWith.toLowerCase();
     }
@@ -77,14 +105,21 @@ function createEndsWithFilter(endsWith) {
  * TIP: You need to loop over the Strings, right? We need to pass each String to 
  * the modify Function, but we need to collect the results into some collection.
  */
+/*
+I: function takes in an array of strings and a function that modifies a string
+O: function returns the modified array of strings
+C: N/A
+E: N/A
+*/
 function modifyStrings(strings, modify) {
     // YOUR CODE BELOW HERE //
-    // create a collection //
+    // create a new array //
     var modified = [];
-    // loop array, pass to function //
+    // iterate through array, pass modified function through string//
     for (var i = 0; i < strings.length; i++) {
         modified.push(modify(strings[i]));
     }
+    // return the output array 
     
     return modified;
     
@@ -100,8 +135,15 @@ function modifyStrings(strings, modify) {
  * 
  * TIP: You need to loop over the Strings, right? And pass them to the test?
  */
+/*
+I: function takes in an array of strings and function that tests the string 
+O: function returns a boolean if strings passed the test
+C: N/A
+E: N/A
+*/
 function allStringsPass(strings, test) {
     // YOUR CODE BELOW HERE //
+    // iterate through the array and pass function through strings
     for (var i = 0; i < strings.length; i++) {
         if (test(strings[i]) === false) {
             return false;
