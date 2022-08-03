c492c5754d43b7fd987bb91381342302be1142ee
Matthew S <matthewjschiro@gmail.com>
2022-08-03 20:38:15 +0000

    matchy data step2

--- projects/matchy/data.js
+++ projects/matchy/data.js
@@ -11,14 +11,36 @@
 //////////////////////////////////////////////////////////////////////
 // Step 1 - Object Creation //////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
+// create empty object called animal
+var animal = {};
+// add property of species and name any species using dot notation
+animal.species = "canine";
+// add property of name using bracket notation and give the animal a name
+animal["name"] = "Puppy";
+// add property called noises and make it an empty array
+animal.noises = [];
+// print animal to console using console.log
+console.log(animal);
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Array Creation ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
+// create empty array called noises
+var noises = [];
+// add a string to noises using bracket notation
+noises[0] = "bark";
+// add another noise to end of noises using array method
+noises.push("woof");
+// add another noise using unshift method
+noises.unshift("howl");
+// add another noise to end of array using bracket notation
+noises[noises.length] = "whine";
+// console log length of noises
+console.log(noises.length);
+// console log last element without hardcoding
+console.log(noises.length - 1);
+// console log the whole array
+console.log(noises);
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Combining Step 1 and 2 ///////////////////////////////////
